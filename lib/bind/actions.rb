3d6e669b83f98eb303645a5a23cdbe74f11ef218
TJ Holowaychuk <tj@vision-media.ca>
2009-03-03 12:34:31 -0800

    - Added action RefreshBrowsersHaml for haml / sass support

--- lib/bind/actions.rb
+++ lib/bind/actions.rb
@@ -2,5 +2,6 @@
 module Bind
   module Actions
     autoload :RefreshBrowsers, 'bind/actions/refresh_browsers'
+    autoload :RefreshBrowsersHaml, 'bind/actions/refresh_browsers_haml'
   end
 end
---
+++ lib/bind/actions/refresh_browsers_haml.rb
@@ -0,0 +1,42 @@
+
+require 'rubygems'
+require 'haml'
+require 'haml/exec'
+
+module Bind
+  module Actions
+    class RefreshBrowsersHaml
+      
+      attr_accessor :browsers, :uri
+      
+      def initialize uri, dest, *browsers
+        @uri, @dest, @browsers = uri, dest, browsers
+      end
+      
+      def call file
+        build_haml file if haml? file
+        build_sass file if sass? file
+        @browsers.each { |browser| `open -g -a #{browser} #{uri}` } 
+      end
+      
+      def build_haml file
+        opts = Haml::Exec::Haml.new [file.path, File.join(@dest, file.path)]
+        opts.parse!
+      end
+      
+      def build_sass file
+        opts = Haml::Exec::Sass.new [file.path, File.join(@dest, file.path)]
+        opts.parse!
+      end
+      
+      def haml?
+        file.path.include? '.haml'
+      end
+      
+      def sass?
+        file.path.include? '.sass'
+      end
+      
+    end
+  end
+end
