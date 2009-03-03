c95c11d7ff807a08cb356acfd6646abfc1e90116
TJ Holowaychuk <tj@vision-media.ca>
2009-03-03 12:35:25 -0800

    - Fixed :log for examples

--- examples/log_changes.rb
+++ examples/log_changes.rb
@@ -5,5 +5,5 @@ require File.dirname(__FILE__) + '/../lib/bind'
 
 style = File.dirname(__FILE__) + '/style.css'
 action = lambda { |file| puts "modified #{file}" } 
-listener = Bind::Listener.new :event => :change, :files => [style], :interval => 1, :action => action, :timeout => 10, :log => $stdout
+listener = Bind::Listener.new :event => :change, :files => [style], :interval => 1, :action => action, :timeout => 10, :debug => $stdout
 listener.run!
--- examples/refresh_browsers.rb
+++ examples/refresh_browsers.rb
@@ -6,5 +6,5 @@ require File.dirname(__FILE__) + '/../lib/bind'
 html  = File.expand_path(File.dirname(__FILE__) + '/demo.html')
 style = File.dirname(__FILE__) + '/style.css'
 action = Bind::Actions::RefreshBrowsers.new html, 'Safari', 'Firefox'
-listener = Bind::Listener.new :event => :change, :files => [style], :interval => 1, :action => action, :timeout => 60, :log => $stdout
+listener = Bind::Listener.new :event => :change, :files => [style], :interval => 1, :action => action, :timeout => 60, :debug => $stdout
 listener.run!
