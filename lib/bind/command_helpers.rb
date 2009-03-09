8f68f73fb6e49c60f0d11fd691dac9eaeaa264ed
TJ Holowaychuk <tj@vision-media.ca>
2009-03-09 10:23:30 -0700

    Added --eval to common options

--- lib/bind/command_helpers.rb
+++ lib/bind/command_helpers.rb
@@ -3,6 +3,7 @@ def set_common_options c
   c.option '-i', '--interval SECONDS', Integer, 'Interval in seconds in which to listen for an event.'
   c.option '-t', '--timeout SECONDS', Integer, 'Timeout after n seconds.'
   c.option '-r', '--require LIBS', Array, 'Require ruby libraries.'
+  c.option '-e', '--eval STRING', String, 'Evaluate a string of Ruby in context of Bind, so the file local variable is available.'
   c.option '-V', '--verbose', 'Log information to STDOUT.'
 end
 
